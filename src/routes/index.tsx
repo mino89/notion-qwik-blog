1fac5c5c1bacfee000a579c15dcd469293a3fd80
mino89 <pierdomenicoreitano@gmail.com>
2023-06-28 17:51:50 +0200

    feat: implemented article reading from notion

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,102 +1,49 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { Resource, component$, useResource$ } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
+import { Client } from '@notionhq/client';
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID as string;
+const queryOptions = {
+  database_id: databaseId,
+}
+
+export async function getDatabase() {
+  const response = await notion.databases.query(queryOptions);
+  return response.results.map((page: any) => (
+    {
+      id: page.id,
+      title: page.properties.Name.title[0]?.text?.content,
+      cover: page.cover?.external.url,
+    }
+  ));
+}
+
 
 export default component$(() => {
+  const database = useResource$<any[]>(async () => {
+    return getDatabase();
+  });
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
+      <h1>Welcome to Qwik</h1>
+      <Resource
+        value={database}
+        onPending={() => <div>Loading...</div>}
+        onRejected={() => <div>Failed to load page</div>}
+        onResolved={(data) => (
+          <> {data.map((page) => (
+            <>
+              <h1>
+              <a href={`/${page.id}`}>{page.title}</a>
+                </h1>
+            </>
+          ))
+          }
           </>
-        </Infobox>
+        )}
+      />
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{' '}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
     </>
   );
 });
@@ -110,3 +57,4 @@ export const head: DocumentHead = {
     },
   ],
 };
+
